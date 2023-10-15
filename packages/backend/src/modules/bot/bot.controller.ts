bf0c2a88591a2ed2e2e4745098e640aa2c8cf236
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-10-15 13:04:47 +0000

    added privacy policy

--- packages/backend/src/modules/bot/bot.controller.ts
+++ packages/backend/src/modules/bot/bot.controller.ts
@@ -14,4 +14,52 @@ import {
 import { BotService } from './bot.service';
 import { CreateBotDto, UpdateBotDto } from '../../dto/bot-config/0.0.1.dto';
 import { SecureRequest } from '../../types/secure-request';
-import { ZodValidationPip
+import { ZodValidationPipe } from '../../pipes/zod-validation/zod-validation.pipe';
+import { botValidation, updateBotValidation } from './valiation.schema';
+
+@Controller('bots')
+export class BotController {
+  constructor(private agentService: BotService) {}
+  @Post()
+  @UsePipes(new ZodValidationPipe(botValidation))
+  async createBot(
+    @Body() body: CreateBotDto,
+    @Request() request: SecureRequest,
+  ) {
+    const creatorId = request.authPayload.uid;
+    return this.agentService.createBot(creatorId, body);
+  }
+
+  @Patch(':id')
+  async updateBot(
+    @Param('id', ParseIntPipe) botId: number,
+    @Body(new ZodValidationPipe(updateBotValidation)) body: UpdateBotDto,
+    @Request() request: SecureRequest,
+  ) {
+    const creatorId = request.authPayload.uid;
+    const bot = await this.agentService.getBotById(botId);
+    if (bot.creatorId !== creatorId || body.id !== botId) {
+      throw new UnauthorizedException();
+    }
+    return this.agentService.updateBot(creatorId, botId, body);
+  }
+
+  @Delete(':id')
+  async deleteBot(
+    @Param('id', ParseIntPipe) botId: number,
+    @Request() request: SecureRequest,
+  ) {
+    const creatorId = request.authPayload.uid;
+    const bot = await this.agentService.getBotById(botId);
+    if (bot.creatorId !== creatorId) {
+      throw new UnauthorizedException();
+    }
+    return this.agentService.deleteBot(botId);
+  }
+
+  @Get(':id')
+  async getBot(
+    @Param('id', ParseIntPipe) botId: number,
+    @Request() request: SecureRequest,
+  ) {
+    const creato
