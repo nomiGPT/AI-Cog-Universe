74111519e5d139f1d3d25231e2fde43f3347a9ac
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-10-22 15:43:59 +0000

    Release v0.0.14

--- packages/backend/src/modules/completion/services/agent.service.ts
+++ packages/backend/src/modules/completion/services/agent.service.ts
@@ -11,4 +11,56 @@ import { Subject } from 'rxjs';
 import { CallbackManager } from 'langchain/callbacks';
 import { ChatHistoryBuilderService } from 'src/services/chat-history-builder/chat-history-builder.service';
 import { BaseChainStream } from './base-chain-stream';
-import
+import { LLMResult } from 'langchain/schema';
+
+@Injectable()
+export class AgentService extends BaseChainStream {
+  private readonly logger = new Logger(AgentService.name);
+
+  constructor(
+    private agent: AgentBuilderService,
+    private chatHistoryBuilder: ChatHistoryBuilderService,
+  ) {
+    super();
+  }
+
+  async getCompletion(
+    question: string,
+    conversation: Conversation,
+    subject: Subject<NewMessage>,
+  ) {
+    const bot = conversation.bot as Bot;
+    if (bot.type !== BotType.AGENT) {
+      throw new InternalServerException('Bot is not an agent');
+    }
+
+    const callbackManager: CallbackManager = CallbackManager.fromHandlers({
+      handleLLMNewToken: (token) => {
+        subject.next({
+          content: token,
+          conversationId: conversation.id,
+          fromType: 'ai',
+          type: 'response-token',
+          fromId: conversation.bot.id,
+        });
+      },
+      handleLLMEnd(output: LLMResult): any {
+        if (!output.generations[0][0]?.text) {
+          return;
+        }
+        subject.next({
+          content: output.generations[0][0]?.text,
+          conversationId: conversation.id,
+          fromType: 'ai',
+          type: 'message',
+          fromId: bot.id,
+        });
+      },
+    });
+
+    const toolsCallbackManager = CallbackManager.fromHandlers({
+      handleToolStart: (_, input: string) => {
+        this.logger.log('handleToolStart', input, JSON.stringify(_, null, 2));
+        subject.next({
+          content: input,
+          conversati
