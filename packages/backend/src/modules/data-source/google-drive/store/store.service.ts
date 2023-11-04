54ab5f0a35bec5246a97156dedea40c597d20b82
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-11-04 07:55:09 +0000

    Code cleanup

--- packages/backend/src/modules/data-source/google-drive/store/store.service.ts
+++ packages/backend/src/modules/data-source/google-drive/store/store.service.ts
@@ -15,4 +15,50 @@ import { SecureRequest } from 'src/types/secure-request';
 export class StoreService {
   constructor(
     @Inject('PARENT_SPLITTER')
-    private readonly splitter: RecursiveCharacterTextSplit
+    private readonly splitter: RecursiveCharacterTextSplitter,
+    @Inject(REQUEST) private req: SecureRequest,
+    private readonly configService: ConfigService,
+    private pineconeService: PineconeService,
+  ) {}
+
+  async storePDFFromBlob(blob: Blob, fileMetadata) {
+    const openAiApiKey = this.configService.get<string>(ENV.OPEN_AI_API_KEY);
+    const pineconeIndex = await this.pineconeService.getIndex();
+    const docs = await this.parsePDFBlob(blob, fileMetadata);
+    console.log('docs', docs);
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: openAiApiKey,
+      modelName: DOC_EMBEDDING_MODEL,
+    });
+    const store = await PineconeStore.fromDocuments(docs, embeddings, {
+      pineconeIndex,
+      namespace: this.req.authPayload.uid,
+    });
+
+    return {
+      success: true,
+    };
+  }
+
+  async parsePDFBlob(blob: Blob, metadata: any) {
+    const loader = new WebPDFLoader(blob);
+    const docs = await loader.load();
+    docs[0].metadata = {
+      ...docs[0].metadata,
+      ...metadata,
+    };
+    const splitDocuments = await this.splitter.splitDocuments(docs);
+    return splitDocuments;
+  }
+
+  // TODO: REMOVE
+  async test() {
+    const openAiApiKey = this.configService.get<string>(ENV.OPEN_AI_API_KEY);
+    const embeddings = new OpenAIEmbeddings({
+      openAIApiKey: openAiApiKey,
+      modelName: DOC_EMBEDDING_MODEL,
+    });
+    const pineconeIndex = await this.pineconeService.getIndex();
+    const store = await PineconeStore.fromExistingIndex(embeddings, {
+      pineconeIndex,
+      namespace: this.req.authPayload.uid
