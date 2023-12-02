66016499df737efe5e9be97dd2dc8ee04b6566b9
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-12-02 11:17:35 +0000

    make it executable

--- packages/backend/src/services/chains/agent/agent.service.ts
+++ packages/backend/src/services/chains/agent/agent.service.ts
@@ -4,4 +4,52 @@ import { Conversation } from '@my-monorepo/shared';
 import { CallbackManager } from 'langchain/callbacks';
 import { ChatHistoryBuilderService } from 'src/services/chat-history-builder/chat-history-builder.service';
 import { AgentLLMBuilder } from 'src/lib/llm-builder/agent-llm-builder';
-import { formatToOpenAITool, SerpAPI, WolframAlphaTool 
+import { formatToOpenAITool, SerpAPI, WolframAlphaTool } from 'langchain/tools';
+import { createAgent } from 'src/lib/chains/agent';
+import * as process from 'process';
+import { DallETool } from 'src/lib/tools/DallE';
+import { OptionsTool } from 'src/lib/tools/ButtonOptions';
+import { RetrievalTool } from 'src/lib/tools/retrieval';
+
+@Injectable()
+export class AgentService extends AgentBuilder {
+  private readonly logger = new Logger(AgentService.name);
+
+  constructor(private chatHistoryBuilder: ChatHistoryBuilderService) {
+    super();
+  }
+
+  // TODO: typesafe this
+  createUIElement(type: string, payload: any) {
+    switch (type) {
+      case 'button_group':
+        return JSON.stringify({
+          type: 'ui',
+          ui_type: 'button_group',
+          payload: payload,
+        });
+    }
+  }
+
+  async fromConversation(
+    conversation: Conversation,
+    callbackManager: CallbackManager,
+    toolsCallbackManager: CallbackManager,
+    uiCallbacks: (ui: string) => void,
+    dalleCallbackManager: CallbackManager,
+    dalleCallback: (imageUrl: string) => void,
+  ) {
+    if (conversation.bot.type !== 'AGENT') {
+      throw new Error('Bot is not an agent');
+    }
+
+    const model = new AgentLLMBuilder().build({
+      lmConfig: conversation.bot.configuration.lm,
+      callbackManager,
+      keys: conversation.creator,
+    });
+    const serpTool = new SerpAPI(process.env.SERP_API_KEY);
+    serpTool.callbacks = toolsCallbackManager;
+    const wolframAlpha = new WolframAlphaTool({
+      appid: process.env.WALPHA_APP_ID,
+      callbacks: toolsCallbackManage
