83fb1e95e65ed7d95d6d3f11d92e335dc4bb5acc
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-12-09 16:47:41 +0000

    tracing: Fix off-by-one on allocating stat->pages

--- packages/backend/src/services/services.module.ts
+++ packages/backend/src/services/services.module.ts
@@ -2,4 +2,29 @@ import { Module } from '@nestjs/common';
 import { PineconeService } from './pinecone/pinecone.service';
 import { PdfSplitterService } from './pdf-splitter/pdf-splitter.service';
 import { DocumentNamespaceService } from './document-namespace/document-namespace.service';
-import { VectorStoreModule } from './vector-store/v
+import { VectorStoreModule } from './vector-store/vector-store.module';
+import { LlmModule } from './llm/llm.module';
+import { EmbeddingsModule } from './embeddings/embeddings.module';
+import { DocumentNamespaceModule } from './document-namespace/document-namespace.module';
+import { PineconeModule } from './pinecone/pinecone.module';
+import { ChainsModule } from './chains/chains.module';
+import { ChatHistoryBuilderModule } from './chat-history-builder/chat-history-builder.module';
+import { AxiosModule } from './axios/axios.module';
+
+const modules = [
+  VectorStoreModule,
+  LlmModule,
+  EmbeddingsModule,
+  DocumentNamespaceModule,
+  PineconeModule,
+  ChainsModule,
+  ChatHistoryBuilderModule,
+  AxiosModule,
+];
+
+@Module({
+  providers: [PineconeService, PdfSplitterService, DocumentNamespaceService],
+  imports: modules,
+  exports: modules,
+})
+export class ServicesModule {}
---
+++ packages/backend/src/services/vector-store/vector-store.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@nestjs/common';
+import {
+  EmbeddingsConfig,
+  EmbeddingsService,
+} from '../embeddings/embeddings.service';
+import { DocumentNamesp
