87e29b5efe94783b385c8fb130078393a2bd710b
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-12-10 17:57:10 +0000

    Prepare 1.0.4 release

---
+++ packages/frontend/next.config.js
@@ -0,0 +1,32 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: '',
+        pathname: '/**'
+      },
+      {
+        protocol: "https",
+        hostname: "oaidalleapiprodscus.blob.core.windows.net",
+        port: '',
+        pathname: '**'
+      },
+    ]
+  },
+  webpack(config) {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+
+    return config;
+  }
+}
+
+module.exports = nextConfig;
