4ef141e907c891a1512dbda0b7ddd52b32b078c8
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-12-17 12:15:11 +0000

    Design updates

---
+++ packages/frontend/src/components/BaseFormFields/ChipsInput/index.tsx
@@ -0,0 +1,3 @@
+import React, {FunctionComponent, useCallback, useMemo, useRef} from 'react';
+import {FieldError, Merge} from "react-hook-form";
+import {XCircleIcon} from "@heroicons/react/24/outline";
