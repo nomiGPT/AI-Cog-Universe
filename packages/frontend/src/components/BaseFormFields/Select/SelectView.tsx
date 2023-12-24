f821ecc04e7a6716c6db56e1d0533e56c5ac2454
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-12-24 18:31:46 +0000

    3.2.8

--- packages/frontend/src/components/BaseFormFields/Select/SelectView.tsx
+++ packages/frontend/src/components/BaseFormFields/Select/SelectView.tsx
@@ -6,4 +6,59 @@ import buttonStyle from "@/components/Button/styles.module.scss";
 import styles from './styles.module.scss';
 import {ChevronDownIcon} from "@heroicons/react/24/solid";
 import Portal from "@/components/Portal";
-import useOverlay fro
+import useOverlay from "@/components/BaseFormFields/Select/use-overlay.hook";
+
+const DEFAULT_PLACEHOLDER = 'Select...';
+
+type Props = {
+  select: UseSelectReturnValue<SelectOption>
+  options: SelectOption[];
+  placeholder?: string;
+  className?: string;
+}
+
+const SelectView: FunctionComponent<Props> = (props) => {
+  const {
+    isOpen,
+    getToggleButtonProps,
+    getMenuProps,
+    highlightedIndex,
+    getItemProps,
+    selectedItem
+  } = props.select;
+
+  const {
+    setReferenceElement,
+    setPopperElement,
+    attributes,
+    popperStyles
+  } = useOverlay();
+
+  return (
+    <div className={clsx(styles.SelectView, props.className)}>
+      <div ref={setReferenceElement}>
+        <button
+          {...getToggleButtonProps()}
+          className={clsx(
+            styles.SelectButton,
+            buttonStyle.button,
+            buttonStyle.primary,
+            isOpen && styles.open
+          )}
+          type={'button'}
+        >
+        <span>
+          {
+            selectedItem
+              ? selectedItem.label
+              : (props.placeholder ?? DEFAULT_PLACEHOLDER)
+          }
+        </span>
+          <ChevronDownIcon className={styles.icon} width={24} height={24}/>
+        </button>
+      </div>
+
+      <Portal target={'select-overlay'}>
+        <div ref={setPopperElement} style={{...popperStyles.popper}} {...attributes.popper}>
+          <ul {...getMenuProps({}, {suppressRefError: true})} className={clsx(styles.list, !isOpen && styles.hidden)}>
+            {isOpen && props.options.map(
