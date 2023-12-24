391b47b022400a07835321e9cb74bec908e07920
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-12-24 11:15:07 +0000

    fix typo in readme

---
+++ packages/frontend/src/components/BaseFormFields/Prompt/index.tsx
@@ -0,0 +1,31 @@
+import React, {forwardRef, FunctionComponent, useImperativeHandle} from 'react';
+import useAutoresizeHook from "@/hooks/use-autoresize.hook";
+import clsx from "clsx";
+import textInputStyles from "../TextInput/styles.module.scss";
+import styles from './styles.module.scss';
+
+type Props = React.TextareaHTMLAttributes<HTMLTextAreaElement> & {
+  placeholders?: string[];
+  hasError?: boolean;
+}
+
+const Prompt: FunctionComponent<Props> = forwardRef<HTMLTextAreaElement, Props>((props, ref) => {
+  const {className, hasError, placeholders, ...rest} = props;
+
+  const [textareaRef, setTextareaRef] = useAutoresizeHook();
+  useImperativeHandle(ref, () => textareaRef!);
+
+  return (
+    <div className={styles.Prompt}>
+      <textarea
+        {...rest}
+        ref={setTextareaRef}
+        className={clsx(
+          className,
+          textInputStyles.textInput,
+          props.hasError && textInputStyles.Error,
+          styles.textarea
+        )}
+      />
+      {placeholders?.length && <div className={styles.placeholders}>
+        <span>Should contain the 
