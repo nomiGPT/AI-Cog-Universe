9cfcb8d057e8143b1f96a6a38cc9f1086dda630e
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2023-12-30 11:38:47 +0000

    SUNRPC: Fix a memory leak in the backchannel code

---
+++ packages/frontend/src/components/BaseFormFields/TextInput/index.tsx
@@ -0,0 +1,23 @@
+import React, {forwardRef} from "react";
+import styles from './styles.module.scss'
+import clsx from "clsx";
+
+type TextInputProps = React.InputHTMLAttributes<HTMLInputElement> & {
+  value?: string;
+  hasError?: boolean;
+};
+
+const TextInput = forwardRef<HTMLInputElement, TextInputProps>((props, ref) => {
+  const { className, hasError, ...rest } = props;
+  return (
+    <div className={className}>
+      <input
+        className={clsx(styles.textInput, hasError && styles.Error)}
+        {...rest}
+        ref={ref}
+      />
+    </div>
+  );
+});
+
+Te
