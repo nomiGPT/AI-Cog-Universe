0a79bef3bf0ceba45a7f1882289530efbebd5023
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-01-21 14:00:11 +0000

    pattern matching

--- packages/frontend/src/components/BotForms/components/AgentConfig/form.schema.ts
+++ packages/frontend/src/components/BotForms/components/AgentConfig/form.schema.ts
@@ -2,4 +2,9 @@ import {z} from "zod";
 
 const schema = z.object({
   llm: z.string().nonempty(),
-  api
+  apiKey: z.string().optional(),
+})
+
+export type InputType = z.infer<typeof schema>
+
+export default schema;
---
+++ packages/frontend/src/components/BotForms/components/BotInfo/index.tsx
@@ -0,0 +1,6 @@
+import React, {FunctionComponent} from 'react';
+import {Controller, UseFormReturn} from "react-hook-form";
+import FormFieldWrapper from "@/components/FormFieldWrapper";
+import TextInput from "@/components/BaseFormFields/TextInput";
+import {InputType} from "./form.schema";
+import SimpleColorPicker from "@/components/BaseFor
