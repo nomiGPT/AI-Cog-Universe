e176840316b1e945ca10bd33681affb13ae40a1b
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-01-28 21:57:26 +0000

    upgrade modules

--- packages/frontend/src/components/BotForms/components/RCBotInfo/index.tsx
+++ packages/frontend/src/components/BotForms/components/RCBotInfo/index.tsx
@@ -14,3 +14,56 @@ import styles from '../RCConfig/styles.module.scss';
 type Props = {
   form: UseFormReturn<{
     botInfo: InputType
+  }>
+}
+
+const BotInfo: FunctionComponent<Props> = (props) => {
+  const {
+    register,
+    control,
+    watch,
+    formState: {errors},
+  } = props.form
+
+  const isBoundToDocument = watch('botInfo.isBoundToDocument');
+
+  return (
+    <section className={styles.CreateBot}>
+      <Controller
+        render={({ field: {onChange, value}}) => (
+          <section className={styles.avatarSelection}>
+            <SimpleColorPicker
+              onChange={onChange}
+              options={COLOR_OPTIONS}
+              selected={value}
+              legend={'Pick the bot\'s look'}
+              error={errors.botInfo?.color}
+            />
+            {value && <Planet size={100} mood="happy" color={value}/>}
+          </section>
+        )}
+        control={control}
+        name={'botInfo.color'}
+        defaultValue={''}
+      />
+      <FormFieldWrapper
+        htmlFor={'bot-name'}
+        label={'Bot Name'}
+        fieldError={errors.botInfo?.name}
+      >
+        <TextInput
+          id={'bot-name'}
+          placeholder={'Provide unique bot name'}
+          hasError={!!errors.botInfo?.name}
+          autoComplete={'off'}
+          {...register('botInfo.name', {required: true})}
+        />
+      </FormFieldWrapper>
+      <FormFieldWrapper
+        htmlFor={'description'}
+        label={'Description'}
+        fieldError={errors.botInfo?.description}
+      >
+        <TextInput
+          id={'description'}
+          placeholder={'Provide a description for yo
