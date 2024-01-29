d2c2223dd6643cf7e1fbd5e0741f55b9205ddf5f
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-01-29 07:54:19 +0000

    Update filter.lua

--- packages/frontend/src/components/BotForms/components/ReviewBotDetails/BotDetails/index.tsx
+++ packages/frontend/src/components/BotForms/components/ReviewBotDetails/BotDetails/index.tsx
@@ -9,4 +9,45 @@ type Props = {
 }
 
 const BotDetails: FunctionComponent<Props> = ({
-  d
+  data,
+                                             }) => {
+  const avatar = data.configuration.avatar;
+  return (
+    <section className={styles.detailsSection}>
+      {avatar.type === BotAvatarType.BOT_AVATAR_EMOTE && <Planet size={100} mood="happy" color={avatar.backgroundColor} />}
+      <div className={styles.botInfo}>
+        <DetailsItem label={'name'} value={data.name} />
+        <DetailsItem label={'bot type'} value={data.type} />
+        {data.description && (
+          <DetailsItem
+            label={'Description'}
+            valueClassName={styles.ellipsis}
+            value={data.description}
+          />
+        )}
+        {data.public && (
+          <DetailsItem
+            label={'public'}
+            valueClassName={styles.ellipsis}
+            value={data.public ? 'yes' : 'no'}
+          />
+        )}
+
+        {data.configuration.type === BotType.CONVERSATIONAL && data.configuration.lm?.prompt && (
+          <DetailsItem
+            label={'custom prompt'}
+            valueClassName={styles.ellipsis}
+            value={data.configuration.lm.prompt}
+          />
+        )}
+        {data.configuration.type === BotType.CONVERSATIONAL && data.configuration.lm?.modelName && (
+          <DetailsItem
+            label={'model'}
+            valueClassName={styles.ellipsis}
+            value={data.configuration.lm.modelName}
+          />
+        )}
+        {data.configuration.type === BotType.CONVERSATIONAL && data.configuration.lm?.apiKey && (
+          <DetailsItem
+            label={'api key'}
+   
