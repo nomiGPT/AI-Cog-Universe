9d88a27a740df4f270bd8fb916cab0ab85e30fcd
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-02-10 18:28:06 +0000

    Wtf

---
+++ packages/frontend/src/components/ConversationElements/index.tsx
@@ -0,0 +1,26 @@
+
+import React, {FunctionComponent} from 'react';
+import {Conversation} from "@my-monorepo/shared";
+import BotSummary from "@/components/ConversationElements/BotSummary";
+import styles from './styles.module.scss'
+import DocumentSummary from "@/components/ConversationElements/DocumentSummary";
+import Portal from "@/components/Portal";
+
+type Props = {
+  conversationElements: Pick<Conversation, 'bot' | 'document'>
+}
+
+const ConversationElements: FunctionComponent<Props> = ({
+  conversationElements: {bot, document}
+                                                 }) => {
+  return (
+    <section className={styles.ConversationElements}>
+      <Portal target={'header-placeholder'}>
+        <BotSummary bot={bot} />
+      </Portal>
+      {document && <DocumentSummary document={document}/>}
+    </section>
+  );
+}
+
+export default ConversationElements;
---
+++ packages/frontend/src/components/ConversationsList/ConversationItem/index.tsx
@@ -0,0 +1,34 @@
+
+import React, {FunctionComponent} from 'react';
+import Link from "next/link";
+import {ConversationItem as Conversation} from "@/types/ChatThread";
+import {ChatBubbleBottomCenterTextIcon} from "@heroicons/react/24/outline";
+import styles from "./styles.module.scss";
+import clsx from "clsx";
+import {useRouter} from "next/router";
+
+type ConversationItemProps = {
+  conversation: Conversation
+}
+
+const ConversationItem: FunctionComponent<ConversationItemProps> = ({conversation}) => {
+  const router = useRouter()
+  const conversationId = router.query.conversationId as string
+
+  return (
+    <Link
+      className={clsx(
+        styles.conversationItem,
+        +conversationId === conversation.id && styles.selected
+      )}
+      href={`/conversations/${conversation.id}`}
+    >
+      <ChatBubbleBottomCenterTextIcon width={24} height={24}/>
+      <span>
+        {conversation.title}
+      </span>
+    </Link>
+  );
+}
+
+export default ConversationItem;
