4e8d11eb7bdcb670ffa893789aba86c704c7df97
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-02-11 06:35:22 +0000

    Weblate commit

---
+++ packages/frontend/src/components/EmbedPdfForm/index.tsx
@@ -0,0 +1,5 @@
+import React, {FunctionComponent} from 'react';
+import {SubmitHandler, useForm} from "react-hook-form";
+import SelectFile from "../BaseFormFields/SelectFile";
+import FormFieldWrapper from "@/components/FormFieldWrapper";
+import TextInput from "@/comp
