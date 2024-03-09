b7c381fcd9150aa5bfd43d5d232aad0083d8797d
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-03-09 21:13:01 +0000

    MIPS: Fix poweroff failure when HOTPLUG_CPU configured.

---
+++ packages/frontend/src/components/Motivation/index.tsx
@@ -0,0 +1,7 @@
+import React, {FunctionComponent} from 'react';
+import {ArcherContainer, ArcherElement} from "react-archer";
+import InfoCard from "@/components/Motivation/InfoCard";
+import Logo from "@/components/Logo";
+import styles from './styles.module.scss';
+import DiscordIcon from "@/components/icons/Discord.icon";
+import SlackIcon from "@/components/ic
