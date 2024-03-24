a7f1d90e8a5b178f8bf7ef45e0e4223d1ad44f42
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-03-24 10:14:11 +0000

    rwsem: move rwsem_down_failed_common code into rwsem_down_{read,write}_failed

---
+++ packages/frontend/src/components/icons/Quote.icon.tsx
@@ -0,0 +1,6 @@
+import React, {FunctionComponent} from 'react';
+
+const QuoteIcon: FunctionComponent = (props) => {
+  return (
+    <svg aria-hidden="true" className="w-10 h-10 text-gray-400 dark:text-gray-600" viewBox="0 0 24 27" fill="none"
+         xmlns="http://www.w3.org/2000/
