a564adf13ca657f9be7a06312d8018be15ca29b1
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-03-24 14:03:22 +0000

    Change composer.json

---
+++ packages/frontend/src/constants/index.ts
@@ -0,0 +1,29 @@
+
+import SelectOption from "@/types/SelectOption";
+import {BotType} from "@my-monorepo/shared";
+
+export const END_COMPLETION = '[DONE]';
+
+export const BOTS_OPTIONS: SelectOption[] = [
+  { value: BotType.CONVERSATIONAL, label: 'Conversational Bot' },
+  { value: BotType.RETRIEVAL_CONVERSATIONAL, label: 'PDF Retrieval Conversational' },
+]
+
+export const LLM_OPTIONS: SelectOption[] = [
+  { value: 'gpt-3.5-turbo', label: 'GPT3.5 turbo' },
+  { value: 'gpt-4', label: 'GPT4' },
+  { value: 'gpt-3.5-turbo-1106', label: 'GPT3.5 (function calling)' },
+  { value: 'gpt-4-1106-preview', label: 'GPT4 (function calling)' }
+];
+
+export const COLOR_OPTIONS = [
+  {label: 'Weldon Blue', value: '#749da1'},
+  {label: 'Sage', value: '#b4be89'},
+  {label: 'Gold', value: '#e0be99'},
+  {label: 'Ruddy Pink', value: '#eb9191'},
+];
+
+export const LOCAL_STORAGE = {
+  TOKEN: 'token',
+  TOKEN_EXPIRES_AT: 'token_expires_at',
+}
---
+++ packages/frontend/src/helpers/api.ts
@@ -0,0 +1,44 @@
+import axios from "axios";
+import {LOCAL_STORAGE} from "@/constants";
+import {toast} from "react-toastify";
+import {HttpStatus} from "@my-monorepo/shared";
+
+const apiInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_BACKEND_API,
+});
+
+
+apiInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem(LOCAL_STORAGE.TOKEN);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
+apiInstance.interceptors.response.use((response) => {
+  return response;
+}, (error) => {
+  if (!error?.response) {
+    if (error) {
+      toast('An Error occurred', {
+        type: "error",
+      });
+      return Promise.reject(error);
+    }
+    return;
+  }
+
+  switch (error.response.status) {
+    case HttpStatus.BAD_REQUEST:
+      toast(error.response.data?.message || "Bad request!", {
+        type: "error",
+      }); break;
+    case HttpStatus.UNPROCESSABLE_ENTITY:
+      toast("Resource cannot be processed!", {
+        type: "error",
+      }); break;
+    case HttpStatus.UNAUTHORIZED:
+      toast("Sorry! You don't h
