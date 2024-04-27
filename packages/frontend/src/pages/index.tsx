b1f8e2df654b218743015afc91d8e485e1e6e011
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-04-27 08:11:35 +0000

    Fixed bugs

--- packages/frontend/src/pages/index.tsx
+++ packages/frontend/src/pages/index.tsx
@@ -3,4 +3,32 @@ import {NextPageWithLayout} from "@/pages/_app";
 import Logo from "@/components/Logo";
 import LinkIconButton from "@/components/Button/IconButton/LinkIconButton";
 import GithubIcon from "@/components/icons/Github.icon";
-import LinkButton from "@/components/Button/LinkButt
+import LinkButton from "@/components/Button/LinkButton";
+import clsx from "clsx";
+import Motivation from "@/components/Motivation";
+import Welcome from "@/components/Welcome";
+import layoutStyles from "@/components/Layouts/DefaultLayout/styles.module.scss";
+import styles from './styles.module.scss';
+
+const Home: NextPageWithLayout = () => {
+  return (
+    <div className={clsx(layoutStyles.container, styles.Home)}>
+      <header>
+        <Logo/>
+        <div className={layoutStyles.spacer}/>
+        <LinkButton href={'/login'}>Login</LinkButton>
+        <LinkIconButton href={'https://github.com/amellouki/cogniverse'}>
+          <GithubIcon/>
+        </LinkIconButton>
+      </header>
+      <main>
+        <Welcome />
+        <Motivation />
+      </main>
+    </div>
+  );
+}
+
+Home.getLayout = (x) => x;
+
+export default Home;
