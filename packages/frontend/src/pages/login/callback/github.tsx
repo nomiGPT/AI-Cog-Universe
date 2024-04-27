6062c09c8383917974a8fca43dbbc1d9e504a199
nomiGPT <135353472+nomiGPT@users.noreply.github.com>
2024-04-27 13:54:51 +0000

    Update MISSION.md

--- packages/frontend/src/pages/login/callback/github.tsx
+++ packages/frontend/src/pages/login/callback/github.tsx
@@ -1,29 +1,28 @@
 import React, {useEffect} from 'react';
-import Login from "@/pages/login";
-import {NextPageWithLayout} from "@/pages/_app";
-import {useRouter} from "next/router";
 import {useSearchParams} from "next/navigation";
-import apiInstance from "@/helpers/api";
+import {useRouter} from "next/router";
+import {NextPageWithLayout} from "@/pages/_app";
+import Login from "@/pages/login";
 import {LOCAL_STORAGE} from "@/constants";
+import apiInstance from "@/helpers/api";
 
-const DiscordCallback: NextPageWithLayout = (props) => {
+const GithubCallback: NextPageWithLayout = (props) => {
   const router = useRouter();
   const params = useSearchParams();
   useEffect(() => {
     const code = params.get('code');
     if (code) {
-      apiInstance.post('/login/discord', {code}).then(res => {
+      apiInstance.post('/login/github', {code}).then(res => {
         localStorage.setItem(LOCAL_STORAGE.TOKEN, res.data.access_token);
         router.push('/conversations').then(() => console.log('Logged in successfully!'));
       })
     }
   }, [params])
   return (
-    <div></div>
+    <div>Logging in...</div>
   );
 }
 
+GithubCallback.getLayout = Login.getLayout;
 
-DiscordCallback.getLayout = Login.getLayout;
-
-export default DiscordCallback;
+export default GithubCallback;
---
+++ packages/frontend/src/pages/login/callback/google.tsx
@@ -0,0 +1,15 @@
+import { NextPageWithLayout } from '@/pages/_app';
+import Login from '@/pages/login';
+import { useRouter } from 'next/router';
+import { useSearchParams } from 'next/navigation';
+import React, { useEffect } from 'react';
+import apiInstance from '@/helpers/api';
+import { LOCAL_STORAGE } from '@/constants';
+
+const Google: NextPageWithLayout = () => {
+  const router = useRouter();
+  const params = useSearchParams();
+  useEffect(() => {
+    const code = params.get('code');
+    if (code) {
+      apiInstance.post('/google/consent', { code }
